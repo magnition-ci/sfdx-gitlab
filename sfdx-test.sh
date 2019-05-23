88a3e2c32845a03a02147299f098ab448cdf8e5c
kymunr <kirsty.munro@cigna.com>
2019-05-23 13:34:03 +0100

    Apex test reports in xml after parsing test id

--- sfdx-test.sh
+++ sfdx-test.sh
@@ -20,7 +20,9 @@ fi
 sfdx force:auth:jwt:grant --clientid $CI_SFDX_CONSUMER_KEY --jwtkeyfile "$CI_PROJECT_DIR/$CI_SFDX_KEY" --username $CI_SFDX_USERNAME --setdefaultdevhubusername -a GitlabCICDExt
 sfdx force:org:create -v GitlabCICDExt -s -f "$CI_PROJECT_DIR/$CI_SFDX_SCRATCH_DEF" -a $CI_SFDX_ORG
 sfdx force:source:push -u $CI_SFDX_ORG
-sfdx force:apex:test:run -u $CI_SFDX_ORG --wait 10 -c --json -r junit | tee result.json | tee result.xml
+sfdx force:apex:test:run -u $CI_SFDX_ORG --wait 10 -c -r json | tee result.json
 cat result.json | python -c "import json, sys; c=reduce(lambda x, y : (x[0]+y[0], x[1]+y[1]),[(x['totalCovered'], x['totalLines']) for x in json.load(sys.stdin)['result']['coverage']['coverage']], (0, 0)); print 'Total Coverage: %f' % (c[0]/float(c[1])*100 if c[1] else 100)"
-cat result.xml
+TEST_RUN_ID=$(python -c "import json, sys; return json.load(sys.stdin)['result']['summary']['testRunId'];")
+sfdx force:apex:test:report -i $TEST_RUN_ID -r junit | tee report.xml
+cat report.xml
 sfdx force:org:delete -u $CI_SFDX_ORG -p
