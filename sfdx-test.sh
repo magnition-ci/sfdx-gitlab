377bc4b061ac242a480748fa22b7779b6b339263
kymunr <kirsty.munro@cigna.com>
2019-05-24 12:16:43 +0100

    Update to include conditional for tests outcome

--- sfdx-test.sh
+++ sfdx-test.sh
@@ -36,10 +36,19 @@ sfdx force:package:install --package $PACKAGEVERSION --wait 10 --targetusername 
 sfdx force:apex:test:run --targetusername installorg --wait 10 --testlevel $TESTLEVEL --wait 10 -c -r json | tee result.json
 cat result.json | python -c "import json, sys; c=reduce(lambda x, y : (x[0]+y[0], x[1]+y[1]),[(x['totalCovered'], x['totalLines']) for x in json.load(sys.stdin)['result']['coverage']['coverage']], (0, 0)); print 'Total Coverage: %f' % (c[0]/float(c[1])*100 if c[1] else 100)"
 TEST_RUN_ID=$(cat result.json | python -c "import json, sys; data = json.load(sys.stdin); print(data['result']['summary']['testRunId']);")
+OUTCOME=$(cat result.json | python -c "import json, sys; data = json.load(sys.stdin); print(data['result']['summary']['outcome']);")
+#CURRENTHOST=$(cat result.json | python -c "import json, sys; data = json.load(sys.stdin); print(data['result']['summary']['outcome']);")
 echo "$TEST_RUN_ID is our test ID to return result.xml"
 mkdir -p testout
 sfdx force:apex:test:report -i $TEST_RUN_ID -r junit -d testout/
-
-# Delete scratch org
+#export $CURRENTHOST
 sfdx force:org:delete --targetusername installorg --noprompt
+
+#if [ ! $OUTCOME = "Passed" ]; then
+#    # Delete scratch org since the pipeline is failed
+#    sfdx force:org:delete --targetusername installorg --noprompt
+#    echo "Deleted our scratch org as our tests and pipeline have failed"
+#    exit 1
+#fi
+
 
